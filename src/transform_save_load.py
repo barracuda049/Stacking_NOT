5307eb06befcb7846c8d1f64479fff9aadbda92c
Matvey Skripkin <Matvey.Skripkin@skoltech.ru>
2023-03-18 23:11:59 +0300

    Implementation of transform data and new samplers data functions

---
+++ src/transform_save_load.py
@@ -0,0 +1,36 @@
+import torch
+from torch.utils.data import Subset, DataLoader
+from torch.utils.data import TensorDataset
+from distributions import Sampler, LoaderSampler
+
+def transform_data(sampler, name, model):
+
+    loader = sampler.loader
+
+    preds = []
+    with torch.no_grad():
+        for batch, _ in loader:
+            pred = model(batch)
+            preds.append(pred)
+
+    new_sampler = torch.cat(preds, dim=0)
+
+    torch.save(new_sampler, name + '.pt')
+
+    
+def new_samplers(path_to_new_data, batch_size = 64, device = 'cuda', test_ratio = 0.1):
+
+    loaded_new_data = torch.load(path_to_new_data)
+
+
+    dataset = TensorDataset(loaded_new_data, torch.zeros(len(loaded_new_data)).view(-1, 1))
+    idx = list(range(len(dataset)))
+
+    test_size = int(len(idx) * test_ratio)
+    train_idx, test_idx = idx[:-test_size], idx[-test_size:]
+    train_set, test_set = Subset(dataset, train_idx), Subset(dataset, test_idx)
+
+    train_sampler = LoaderSampler(DataLoader(train_set, shuffle=True, num_workers=8, batch_size=batch_size), device = device)
+    test_sampler = LoaderSampler(DataLoader(test_set, shuffle=True, num_workers=8, batch_size=batch_size), device = device)
+
+    return train_sampler, test_sampler
